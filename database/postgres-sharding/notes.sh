fbb0c5063b10c5036104c63adccf1784d64afa2b
sanju <sanjeev@streamsets.com>
2024-05-30 14:37:55 +1200

    postgres sharding notes

--- database/postgres-sharding/notes.sh
+++ database/postgres-sharding/notes.sh
@@ -10,3 +10,16 @@ docker exec -it pg_shard_1 psql -U postgres
 # create pgadmin container
 
 docker run --name pgadmin -p 5555:80 -e 'PGADMIN_DEFAULT_EMAIL=postgres@postgres.com' -e 'PGADMIN_DEFAULT_PASSWORD=postgres' -d dpage/pgadmin4
+
+                        ##  PG SHARDING with Node.js ##
+
+# initialize npm project
+npm init -y
+# packages needed for running node js code
+npm install express pg hashring crypto
+
+# example POST request
+http://localhost:8081/?url=https://wikipedia.com/sharding
+
+# test in chrome console
+fetch("http://localhost:8081/?url=https://wikipedia.com/sharding", {"method":"POST"}).then(a=>a.json()).then(console.log)
